ef68e18d4a5f13f25bae5974a37e821913a8ad3f
Akeam Williams <akeam.williams@mymona.uwi.edu>
2019-04-07 09:37:06 +0000

    create link between frontend and backend

--- app/views.py
+++ app/views.py
@@ -3,16 +3,11 @@ from flask import render_template, request, redirect, url_for, flash
 
 @app.route("/", methods=['GET', 'POST'] )
 def home():
-    if request.method == "POST":
-        details = request.form
-        firstName = details['fname']
-        lastName = details['lname']
-        cur = mysql.connection.cursor()
-        cur.execute("INSERT INTO MyUsers(firstName, lastName) VALUES (%s, %s)", (firstName, lastName))
-        mysql.connection.commit()
-        cur.close()
-        return 'success'
-    return render_template("home.html")
+    cur = mysql.connection.cursor()
+    cur.execute("SELECT firstName, lastName FROM test.MyUsers")
+    rv = cur.fetchall()
+    
+    return render_template("home.html",data=rv[0][0],datas=rv[0][1])
 
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0", port="8080")
