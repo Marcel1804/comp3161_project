f6b29f1059e79f6f3cfa7e8da81854a90d9f8b34
Jake Samuels <crazy_jake74@yahoo.com>
2019-04-06 20:29:03 -0500

    error checking

--- sqlGen.py
+++ sqlGen.py
@@ -22,6 +22,7 @@ columns = {
      
 }
 
+
 class databaseGenerator:
     def __init__(self, dbname, columns):
         self.dbname   = dbname
@@ -34,28 +35,37 @@ class databaseGenerator:
     #NB: if the datatype in the db is varchar the arrib
     #value in the list should be represented as a quoted string
     def addRecord(self, attrib, name):
-        strstatement  =""
-        strstatement  ="INSERT INTO "+str(name)+" "+ "".join(self.column[name])
-        strstatement += ' VALUES({});'.format(','.join("{0}".format(x) for x in attrib))
-        self.mycursor.execute(strstatement)
-
+        try:
+            strstatement  =""
+            strstatement  ="INSERT INTO "+str(name)+" "+ "".join(self.column[name])
+            strstatement += ' VALUES({});'.format(','.join("{0}".format(x) for x in attrib))
+            self.mycursor.execute(strstatement)
+        except:
+            print("Please check input for addRecord")
 
     #key is the identifier of the value to be removed
     #name is the name of the table to removed the record form
     #coltocompare is the name of the column to compare primkey against
     #NB: primkey must be a quoted string if datatype is varchar 
     def removeRecord(self, key, name, coltocompare):
-        strstatement  = ""
-        strstatement  = "DELETE FROM "+str(name)
-        strstatement += " WHERE {} = {};".format(coltocompare, key)
-        self.mycursor.execute(strstatement)
+        try:
+            strstatement  = ""
+            strstatement  = "DELETE FROM "+str(name)
+            strstatement += " WHERE {} = {};".format(coltocompare, key)
+            self.mycursor.execute(strstatement)
+        except:
+            print("Please check input for removeRecord")
 
     #name of table must be python string
-    #NT
     def showTableAll(self, tableName):
-        strstatement = ""
-        strstatement = "SELECT * FROM {};".format(tableName)
-        self.mycursor.execute(strstatement)
+        try:
+            strstatement = ""
+            strstatement = "SELECT * FROM {};".format(tableName)
+            self.mycursor.execute(strstatement)
+            records = self.mycursor.fetchall()
+            return records
+        except:
+            print("Please check input for showTableAll")
 
     #key must be quoted string if the datatype in the database is varchar or date
     #tableName - the name of the table to be updated
@@ -64,37 +74,59 @@ class databaseGenerator:
     #key - the unique identifier of the record
     #NT
     def showTableCondition(self, tableName, coltocompare, key, selection):
-        strstatement = ""
-        strstatement = "SELECT {} FROM {} WHERE {} = {};".format(selection, tableName, coltocompare, key)
-        self.mycursor.execute(strstatement)
-
+        try:
+            strstatement = ""
+            strstatement = "SELECT {} FROM {} WHERE {} = {};".format(selection, tableName, coltocompare, key)
+            self.mycursor.execute(strstatement)
+            records = self.mycursor.fetchall()
+            return records
+        except:
+            print("Please check input for showTableCondition")
+        
     #key must be quoted string if the datatype in the database is varchar or date
     #tableName - the name of the table to be updated
     # columntoupdate - name of column to be updated
     # value - is the new value of the column
     #key - the unique identifier of the record
     #idfn - the column to compare to key in order to find specified record
-    #NT
     def updateRecord(self, tableName, columntoupdate, key, value, idfn ):
-        strstatement = ""
-        strstatement = "UPDATE {} SET {} = {} WHERE {} = {};".format(tableName, columntoupdate, value, idfn, key )
-        self.mycursor.execute(strstatement)
+        try:
+            strstatement = ""
+            strstatement = "UPDATE {} SET {} = {} WHERE {} = {};".format(tableName, columntoupdate, value, idfn, key )
+            self.mycursor.execute(strstatement)
+        except:
+            print("Pease check input for updateRecord")
+
 
     #tableName - the name of the table to be updated
     # column - name of column to be updated
     # value - is the new value of the column 
-    #NT
     def updateTable(self, tableName, column, value):
-        strstatement = ""
-        strstatement = "UPDATE {} SET {} = {};".format(tableName, column, value)
-        self.mycursor.execute(strstatement)
+        try:
+            strstatement = ""
+            strstatement = "UPDATE {} SET {} = {};".format(tableName, column, value)
+            self.mycursor.execute(strstatement)
+        except:
+            print("Please check input for updateTable")
 
+    #destructor closes database and connection
+    def __del__(self): 
+        self.mycursor.close()
+        self.mydb.close()
 
 
 if __name__ == "__main__":
     db = None
     db = databaseGenerator("compustore", columns)
+    
     #testing code
+    #tb = db.showTableCondition("test", "Username", "\"popii\"", "Password")
+    #print(tb)
+    #t  = db.showTableAll("test")
+    #print(t)
+    #db.updateRecord("managers", "person_name", "\"john\"","\"Grim\"", "person_name") 
+    #db.updateTable("test", "id", "1234*2")
+
     #db.addRecord(["\"1234\"", "\"hviujk\"", "\"gdfku\"","\"hjfgf\""], "test")
     #db.addRecord(["\"john\"", "\"samuels\""], "managers")
     #n.addRecord([7564, 657437,], "creditcard")
